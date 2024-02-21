fd298c53fc72d545f29a1f906e56306541eb3b28
Derick Florencio <derickfelix@Dericks-MBP.lan>
2024-02-21 20:08:09 +0000

    feat: setup android platform

---
+++ android/app/src/main/java/com/github/derickfelix/developertools/MainActivity.java
@@ -0,0 +1,5 @@
+package com.github.derickfelix.developertools;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- resources/script.js
+++ resources/script.js
@@ -1,31 +1,31 @@
 const fs = require('fs');
 
-// const SOURCE_ANDROID_ICON = 'resources/android/icon/';
-// const TARGET_ANDROID_ICON = 'android/app/src/main/res/';
+const SOURCE_ANDROID_ICON = 'resources/android/icon/';
+const TARGET_ANDROID_ICON = 'android/app/src/main/res/';
 
 const SOURCE_IOS_ICON = 'resources/ios/icon/';
 const TARGET_IOS_ICON = 'ios/App/App/Assets.xcassets/AppIcon.appiconset/';
 const SOURCE_IOS_SPLASH = 'resources/ios/splash/';
 const TARGET_IOS_SPLASH = 'ios/App/App/Assets.xcassets/Splash.imageset/';
 
-// const ANDROID_ICONS = [
-//   { source: 'drawable-ldpi-icon.png', target: 'drawable-hdpi-icon.png' },
-//   { source: 'drawable-mdpi-icon.png', target: 'mipmap-mdpi/ic_launcher.png' },
-//   { source: 'drawable-mdpi-icon.png', target: 'mipmap-mdpi/ic_launcher_round.png' },
-//   { source: 'drawable-mdpi-icon.png', target: 'mipmap-mdpi/ic_launcher_foreground.png' },
-//   { source: 'drawable-hdpi-icon.png', target: 'mipmap-hdpi/ic_launcher.png' },
-//   { source: 'drawable-hdpi-icon.png', target: 'mipmap-hdpi/ic_launcher_round.png' },
-//   { source: 'drawable-hdpi-icon.png', target: 'mipmap-hdpi/ic_launcher_foreground.png' },
-//   { source: 'drawable-xhdpi-icon.png', target: 'mipmap-xhdpi/ic_launcher.png' },
-//   { source: 'drawable-xhdpi-icon.png', target: 'mipmap-xhdpi/ic_launcher_round.png' },
-//   { source: 'drawable-xhdpi-icon.png', target: 'mipmap-xhdpi/ic_launcher_foreground.png' },
-//   { source: 'drawable-xxhdpi-icon.png', target: 'mipmap-xxhdpi/ic_launcher.png' },
-//   { source: 'drawable-xxhdpi-icon.png', target: 'mipmap-xxhdpi/ic_launcher_round.png' },
-//   { source: 'drawable-xxhdpi-icon.png', target: 'mipmap-xxhdpi/ic_launcher_foreground.png' },
-//   { source: 'drawable-xxxhdpi-icon.png', target: 'mipmap-xxxhdpi/ic_launcher.png' },
-//   { source: 'drawable-xxxhdpi-icon.png', target: 'mipmap-xxxhdpi/ic_launcher_round.png' },
-//   { source: 'drawable-xxxhdpi-icon.png', target: 'mipmap-xxxhdpi/ic_launcher_foreground.png' }
-// ];
+const ANDROID_ICONS = [
+  { source: 'drawable-ldpi-icon.png', target: 'drawable-hdpi-icon.png' },
+  { source: 'drawable-mdpi-icon.png', target: 'mipmap-mdpi/ic_launcher.png' },
+  { source: 'drawable-mdpi-icon.png', target: 'mipmap-mdpi/ic_launcher_round.png' },
+  { source: 'drawable-mdpi-icon.png', target: 'mipmap-mdpi/ic_launcher_foreground.png' },
+  { source: 'drawable-hdpi-icon.png', target: 'mipmap-hdpi/ic_launcher.png' },
+  { source: 'drawable-hdpi-icon.png', target: 'mipmap-hdpi/ic_launcher_round.png' },
+  { source: 'drawable-hdpi-icon.png', target: 'mipmap-hdpi/ic_launcher_foreground.png' },
+  { source: 'drawable-xhdpi-icon.png', target: 'mipmap-xhdpi/ic_launcher.png' },
+  { source: 'drawable-xhdpi-icon.png', target: 'mipmap-xhdpi/ic_launcher_round.png' },
+  { source: 'drawable-xhdpi-icon.png', target: 'mipmap-xhdpi/ic_launcher_foreground.png' },
+  { source: 'drawable-xxhdpi-icon.png', target: 'mipmap-xxhdpi/ic_launcher.png' },
+  { source: 'drawable-xxhdpi-icon.png', target: 'mipmap-xxhdpi/ic_launcher_round.png' },
+  { source: 'drawable-xxhdpi-icon.png', target: 'mipmap-xxhdpi/ic_launcher_foreground.png' },
+  { source: 'drawable-xxxhdpi-icon.png', target: 'mipmap-xxxhdpi/ic_launcher.png' },
+  { source: 'drawable-xxxhdpi-icon.png', target: 'mipmap-xxxhdpi/ic_launcher_round.png' },
+  { source: 'drawable-xxxhdpi-icon.png', target: 'mipmap-xxxhdpi/ic_launcher_foreground.png' }
+];
 
 const IOS_ICONS = [
   { source: 'icon-1024.png', target: 'AppIcon-512@2x.png' }
@@ -48,7 +48,7 @@ function copyImages(sourcePath, targetPath, images) {
   }
 }
 
-// copyImages(SOURCE_ANDROID_ICON, TARGET_ANDROID_ICON, ANDROID_ICONS);
+copyImages(SOURCE_ANDROID_ICON, TARGET_ANDROID_ICON, ANDROID_ICONS);
 
 copyImages(SOURCE_IOS_ICON, TARGET_IOS_ICON, IOS_ICONS);
 copyImages(SOURCE_IOS_SPLASH, TARGET_IOS_SPLASH, IOS_SPLASHES);
